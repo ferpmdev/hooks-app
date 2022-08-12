6089647e8eea33fdc420e4db54d937b924f0e945
Fernando Martinez <fmartinez@agea.com.ar>
2022-08-12 14:07:15 -0300

    fin clase 6 useReducers

--- src/Components/Characters.jsx
+++ src/Components/Characters.jsx
@@ -1,7 +1,25 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useReducer } from "react"
+
+const initialState = {
+  favorites: []
+}
+
+const favoriteReducers = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+    default:
+      return state;
+  }
+}
 
 const Characters = () => {
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReducers, initialState);
+
   const API = "https://rickandmortyapi.com/api"
   
   useEffect(() => {
@@ -9,10 +27,25 @@ const Characters = () => {
     .then(response => response.json())
     .then(data => setCharacters(data.results))
   }, []);
+
+  const handleClick = favorite => {
+    dispatch({type: 'ADD_TO_FAVORITE', payload: favorite})
+
+  }
   
   return (
     <div className="characters">
-      {characters && characters.map(character=>(<h5>{character.name}</h5>))}
+      {favorites.favorites.map(favorite =>(
+        <li key={favorite.id}>
+          {favorite.name}
+        </li>
+      ))}
+      {characters && characters.map(character=>(
+        <div key={character.id} >
+          <h5>{character.name}</h5>
+          <button type="button" onClick={() => handleClick(character)}>Add to favorites</button>
+        </div>
+      ))}
     </div>
   )
 }
