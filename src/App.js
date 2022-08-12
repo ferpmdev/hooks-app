b82a8d1c2bd089b79df56d2a3f4512cd69cbfea8
Fernando Martinez <fmartinez@agea.com.ar>
2022-08-12 12:16:23 -0300

    fin clase 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useContext } from "react";
 import "./App.css";
 import Characters from "./Components/Characters";
 import Header from "./Components/Header";
+import ThemeContext from "./Context/ThemeContext";
 
 function App() {
+  const color = useContext(ThemeContext)
+
   return (
-    <div className="App">
+    <div className="App" style={{color}} >
       <Header />
-      <h1>hola mundo</h1>
       <Characters />
     </div>
   );
